82aeee138dac67480d9ff4fc6db8976e3ca8e0ec
elipauly <elpauly@uiowa.edu>
2026-04-19 01:36:11 -0500

    labelling csvs

--- splitting_data_for_labelling.py
+++ splitting_data_for_labelling.py
@@ -1,15 +1,39 @@
 import pandas as pd
 
-#method: sampling 200 from problemgambling to learn from problem gambling language. 
-#100 of the instances are keyword based, i searched for comments containing "bet", "gamble", "sportsbook", "sportsbetting" and then sampled 100 from those. the other 100 are randomly sampled from the problem gambling subreddit without keyword filtering, to get a more representative sample of the language used in that subreddit.
-#300 are from sportsbook and sportsbetting to learn from those subreddits.
+#method: random bootstrap sampling 200 from gamblingaddiction to learn from problem gambling language. from a skim, this one is more applicable to us than problemgamblign 
+#400 are random bootstrap sampled from sportsbook and sportsbetting to learn from those subreddits.
 
 
-df = pd.read_csv('bert_sportsbook.csv')
-df2 = pd.read_csv('bert_sportsbetting.csv')
+book = pd.read_csv('bert_sportsbook.csv')
+betting = pd.read_csv('bert_sportsbetting.csv')
+problem = pd.read_csv('./misc_testing_ignore/bert_gamblingaddiction.csv')
 
-row_count = len(df)
-row_count2 = len(df2)
+row_count = len(book)
+row_count2 = len(betting)
+row_count3 = len(problem)
 print(f"Total rows: {row_count}")
 print(f"Total rows: {row_count2}")
+print(f"Total rows: {row_count3}")
 
+
+df1 = book.sample(n=200, replace=True)
+df2 = betting.sample(n=200, replace=True)
+df3 = problem.sample(n=200, replace=True)
+
+dfs = [df1, df2, df3]
+
+# 1. Take a sample from each and combine them
+combined_samples = pd.concat(dfs, ignore_index=True)
+combined_samples.to_csv("BERT_Training_Unlabelled_Full.csv", index=False)
+
+
+shuffled = combined_samples.sample(frac=1)
+
+nathan = shuffled.iloc[:287, :]  # First 287 rows
+tristan = shuffled.iloc[287:467, :]  # Next 225 rows, first 512 columns
+pauly = shuffled.iloc[467:, :]  # 
+
+
+nathan.to_csv("nathan.csv", index=False)
+tristan.to_csv("tristan.csv", index=False)
+pauly.to_csv("pauly.csv", index=False)
